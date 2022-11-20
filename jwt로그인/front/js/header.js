efa484d8f87d19814ddec573382292ab40f54b2e
KKU <dkdlwkrsbxjs@naver.com>
2022-11-21 01:48:59 +0900

    2022.11.21 01:48 토큰 검증 api 요청 오류 해결

--- jwt로그인/front/js/header.js
+++ jwt로그인/front/js/header.js
@@ -12,6 +12,9 @@
 
 */
 
+// let url = "http://127.0.0.1:3001";
+axios.defaults.baseURL = 'http://127.0.0.1:3001'
+
 // 1. 로컬스토리지에서 x-access-token 확인
 const jwt = localStorage.getItem("x-access-token");
 setHeader(jwt);
@@ -28,7 +31,7 @@ async function setHeader(jwt) {
   // 2. 토큰 검증 API 요청
   const jwtReturn = await axios({
     method: "get", // http method
-    url: url + "/jwt",
+    url: "/jwt",
     headers: { "x-access-token": jwt }, // packet header
     data: {}, // packet body
   });
